7fbd46a3c6a974a441fe3233182f10c812a49c9f
igardner23 <igardner23@users.noreply.github.com>
2023-03-01 14:03:23 +0000

    ✅ Ready to clone and code.

--- minx0/ext/commands.py
+++ minx0/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from minx0.ext.auth import create_user
+from minx0.ext.database import db
+from minx0.models import Product
 
 
 def create_db():
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from minx0 import create_app
+from minx0.ext.commands import populate_db
+from minx0.ext.database import db
 
 
 @pytest.fixture(scope="session")
